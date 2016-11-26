26d348f14c88e6778e60b0a3e3f3fc50761e6063
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2016-11-26 23:10:43 +0530

    Component refactor.

--- app/component/header.js
+++ app/component/header.js
@@ -1,19 +1,19 @@
 var React = require('react');
 
-var Header = React.createClass({
-	render: function() {
-		return(
-			<nav className='navbar navbar-inverse' style={{margin:'0px',borderRadius:'0px !important'}} >
-				<div className='container-fluid' >
-					<div className='navbar-header' >
-						<a className='navbar-brand' >
-							<span style={{color:'white',fontSize:'25px',letterSpacing:'1px'}} >Search2Read</span>
-						</a>
-					</div>
+var styles = require('../styles');
+
+function Header() {
+	return(
+		<nav className='navbar navbar-inverse' style={styles.navBar}>
+			<div className='container-fluid'>
+				<div className='navbar-header'>
+					<a className='navbar-brand'>
+						<span style={styles.navTitle}>Search2Read</span>
+					</a>
 				</div>
-			</nav>
-			)
-	}
-});
+			</div>
+		</nav>
+	)
+}
 
 module.exports = Header;
---
+++ app/styles/index.js
@@ -0,0 +1,13 @@
+var styles = {
+  navBar: {
+    margin: '0px',
+    borderRadius: '0px'
+  },
+  navTitle: {
+    color: 'white',
+    fontSize: '25px',
+    letterSpacing: '1px'
+  }
+}
+
+module.exports = styles;
